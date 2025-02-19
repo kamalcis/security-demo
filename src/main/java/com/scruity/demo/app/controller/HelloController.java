2759f8ccd8a92c3fa87aedf8448d615519db45c7
kamal <kamalcis@gmail.com>
2025-02-19 14:21:42 +0600

    Initial security demo

---
+++ src/main/java/com/scruity/demo/app/controller/HelloController.java
@@ -0,0 +1,55 @@
+package com.scruity.demo.app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+public class HelloController {
+    
+    List<String> list = new ArrayList<>();
+    
+    @GetMapping("/api/public/hello")
+    public String hello(HttpServletRequest req){
+         return "Hello everyone ! "+ req.getRequestedSessionId();
+    }
+    
+    @GetMapping("/api/admin/hello")
+    public String helloAdmin(HttpServletRequest req){
+        return "Hello Admin ! " + req.getRequestedSessionId();
+    }
+
+    @GetMapping("/api/user/hello")
+    public String helloUser(){
+        return "Hello User";
+    }
+
+    @GetMapping("/api/public/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest req){
+        return (CsrfToken) req.getAttribute("_csrf");
+    }
+
+    
+    @PostMapping("/api/admin/save")
+    public List<String> saveData(@RequestBody String data) {
+        
+        list.add(data);
+        return list;
+        
+    }
+    
+   
+
+}
---
+++ src/main/java/com/scruity/demo/app/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.scruity.demo.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+// import org.springframework.security.core.userdetails.User;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.core.userdetails.UserDetailsService;
+// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+ 
+    @Bean
+    public SecurityFilterChain seqSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        return http.csrf(customizer-> customizer.disable())
+                .authorizeHttpRequests(request-> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                
+                .build();
+
+    }
+
+    // @Bean
+    // UserDetailsService userDetailsService(){
+    //     UserDetails user1 = User.withDefaultPasswordEncoder()
+    //                         .username("kiran")
+    //                         .password("kiran")
+    //                         .roles("USER")
+    //                         .build();
+    //     UserDetails user2 = User.withDefaultPasswordEncoder()
+    //                         .username("adam")
+    //                         .password("adam")
+    //                         .roles("ADMIN")
+    //                         .build();
+    //     return new InMemoryUserDetailsManager(user1,user2);
+    // }
+    
+   @Bean
+    public AuthenticationProvider daoAuthenticationProvider(){
+            
+        return null;
+    }
+}
